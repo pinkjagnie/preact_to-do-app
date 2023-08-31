15b84591da6e7af82b6e7a31bb2e03096cf07155
pinkjagnie <olga.krzysztofik@gmail.com>
2023-08-31 16:50:43 +0200

    Implement adding new to do

--- pages/components/Main.jsx
+++ pages/components/Main.jsx
@@ -7,14 +7,34 @@ class Main extends Component {
     newTodoText: "",
   };
 
+  newTodoChangeHandler = (e) => {
+    this.setState({ newTodoText: e.target.value });
+    console.log("klik");
+  };
+
+  addTodoHandler = () => {
+    const { todos, newTodoText } = this.state;
+    if (newTodoText) {
+      this.setState({
+        todos: [...todos, newTodoText],
+        newTodoText: "",
+      });
+    }
+    console.log("adding " + newTodoText);
+  };
+
   render() {
     const { todos, newTodoText } = this.state;
 
     return (
       <div>
         <h1>To Do List</h1>
-        <input type="text" />
-        <button>Add</button>
+        <input
+          type="text"
+          value={newTodoText}
+          onChange={this.newTodoChangeHandler}
+        />
+        <button onClick={this.addTodoHandler}>Add</button>
         <div>
           <h2>Things I have to do</h2>
           <div>
